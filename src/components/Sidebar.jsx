7016912262459bab2a03ca2a47a98403b7928126
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-09-19 11:26:35 +0400

    upload component files and add Sidebar componenet

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import Footer from "./Footer";
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
