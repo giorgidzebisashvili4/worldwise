73fff2514a6c051ea7ba534764f8c8f2aca55d2e
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-09-30 17:24:45 +0400

    fixed typo

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -2,21 +2,21 @@ import { createContext, useReducer, useState } from "react";
 
 const AuthContext = createContext();
 function reducer(state, action) {
-    switch (action.type) {
-        case "login":
-            retrun { ...state, user: action.payload, isAuthenticated: true };
-        case "logout":
-            return { ...state, user: null, isAuthenticated: false };
-        default:
-            throw new Error("Unknown action type");
-    }
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action type");
+  }
 }
 const FAKE_USER = {
-    name: "Jack",
-    email: "jack@example.com",
-    password: "qwerty",
-    avatar: "https://i.pravatar.cc/100?u=zz",
-  };
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
@@ -31,7 +31,11 @@ function AuthProvider({ children }) {
   function logout() {
     dispatch({ type: "logout" });
   }
-  return <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 function useAuth() {
   const context = useContext(AuthContext);
